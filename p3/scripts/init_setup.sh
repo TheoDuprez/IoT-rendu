af4b211dbd724eb31bbeb61e9e131342e22bd6ff
tgellon <tanguy.gellon@gmail.com>
2025-10-16 11:57:52 +0200

    [FIX] P3: Changed link for docker install

--- p3/scripts/init_setup.sh
+++ p3/scripts/init_setup.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-echo $'Updating packages and installing wget...\n'
+echo $'Updating packages and installing curl...\n'
 sudo apt-get update
-sudo apt-get install -y wget
+sudo apt-get install -y curl
 
-echo $'Installing docker...\n'
-sudo apt-get update
-sudo apt-get install ca-certificates curl
+echo $'\nInstalling docker...'
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc]  https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-echo $'Checking correct installation of docker...\n'
+echo $'\nChecking correct installation of docker...'
 sudo docker --version
 
-echo $'Installing k3d...\n'
+echo $'\nInstalling k3d...'
 sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 sudo chmod +x /usr/local/bin/k3d
 
-echo $'Checking correct installation of k3d...\n'
+echo $'\nChecking correct installation of k3d...'
 k3d --version
 
-echo $'Installing kubectl...\n'
+echo $'\nInstalling kubectl...'
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-echo $'Checking correct installation of kubectl...\n'
+echo $'\nChecking correct installation of kubectl...'
 kubectl version --client
